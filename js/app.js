4cf9c1387460f433b7403c4c28c3a511df647d80
KhomsiAdam <khomsiadam@gmail.com>
2021-11-30 10:44:56 +0100

    Init folder/files structure

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// =============== //
+// Modules imports //
+// =============== //
+
+/* Detection Module */
+// import { elementInView, getElementsInView } from "./modules/detection.js";
+
+// === //
+// App //
+// === //
+
+// Any code that needs to run after the document loads
+document.addEventListener('DOMContentLoaded', () => {
+})
+
+// Any code that needs to run after the document fully loads with all the assets
+window.addEventListener('load', () => {
+})
